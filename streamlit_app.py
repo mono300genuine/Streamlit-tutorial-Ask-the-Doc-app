baf1ea358b67df695ffcac695561f4092b5191d8
mono300genuine <monolinker31@gmail.com>
2024-07-06 05:36:40 +0900

    feat: back to txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.set_page_config(page_title='🦜🔗 Ask the Doc App')
 st.title('🦜🔗 Ask the Doc App')
 
 # File upload
-uploaded_file = st.file_uploader('Upload an article', type='pdf')
+uploaded_file = st.file_uploader('Upload an article', type='txt')
 # Query text
 query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
